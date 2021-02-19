1c344c32a3cfb7b29b1603c8c63163d7e170789b
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-02-19 17:20:19 -0500

    ADD: layout pokemon list

--- src/data.js
+++ src/data.js
@@ -3,11 +3,6 @@ export const filterPokemonsByType = (typePokemon, getPokemon) => {
   
   const dataType = getPokemon.filter((poke)=>{
     return poke.type.includes(typePokemon.value)
-    // console.log(poke.type);
-    // let dataType = poke.type
-    // console.log(dataType);
-    // let newArrayType = dataType.filter(type => type === typePokemon.value)
-    // console.log(newArrayType);
   })
   return dataType
 };
--- src/main.js
+++ src/main.js
@@ -7,23 +7,19 @@ const typePokemon = document.querySelector(".typePokemon");
 
 const getPokemons = data.pokemon;
 
-// console.log(pokemonFiltro.value);
+const renderPokemonsAll = () =>{
+  
+}
 
 const searchPokemonsByTypes = () => {
-  
-  showPokemons(filterPokemonsByType(typePokemon, getPokemons))
+  showPokemons(filterPokemonsByType(typePokemon, getPokemons));
 };
 
-// Event Change
-typePokemon.addEventListener("change", searchPokemonsByTypes);
-
-
 const showPokemons = (pokemonData) => {
+  
   let pokemos = pokemonData.forEach((poke) => {
-    // tbody.innerHTML = ""
     let tbody = document.createElement("tbody");
     tbody.innerHTML = `
-    <tbody >
     <tr>
       <td>
         <p>#${poke.num}</p>
@@ -31,9 +27,10 @@ const showPokemons = (pokemonData) => {
         <p>${poke.name}</p>
       </td>
     </tr>
-  </tbody>
     `;
     pokemonList.appendChild(tbody);
   });
 };
-// showPokemons(getPokemons);
+
+// Event Change
+typePokemon.addEventListener("change", searchPokemonsByTypes);
