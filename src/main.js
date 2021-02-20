6d7df87b1b9693e17dc1059d92ca1cf7f17fc4dd
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-02-20 00:41:58 -0500

    ADD: layout pokemon list right

--- src/main.js
+++ src/main.js
@@ -15,22 +15,22 @@ const searchPokemonsByTypes = () => {
   showPokemons(filterPokemonsByType(typePokemon, getPokemons));
 };
 
-const showPokemons = (pokemonData) => {
+// const showPokemons = (pokemonData) => {
   
-  let pokemos = pokemonData.forEach((poke) => {
-    let tbody = document.createElement("tbody");
-    tbody.innerHTML = `
-    <tr>
-      <td>
-        <p>#${poke.num}</p>
-        <img src=${poke.img} alt=Logo ${poke.name}>
-        <p>${poke.name}</p>
-      </td>
-    </tr>
-    `;
-    pokemonList.appendChild(tbody);
-  });
-};
+//   let pokemos = pokemonData.forEach((poke) => {
+//     let tbody = document.createElement("tbody");
+//     tbody.innerHTML = `
+//     <tr>
+//       <td>
+//         <p>#${poke.num}</p>
+//         <img src=${poke.img} alt=Logo ${poke.name}>
+//         <p>${poke.name}</p>
+//       </td>
+//     </tr>
+//     `;
+//     pokemonList.appendChild(tbody);
+//   });
+// };
 
 // Event Change
 typePokemon.addEventListener("change", searchPokemonsByTypes);
