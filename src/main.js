1d040b1f3fb4951638e33161cd9c4cc6cea85489
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-02-23 00:30:53 -0500

    ADD: Array of 9 pokemons

--- src/main.js
+++ src/main.js
@@ -4,33 +4,43 @@ import data from "./data/pokemon/pokemon.js";
 // Selectores
 const pokemonList = document.querySelector("#pokemonList");
 const typePokemon = document.querySelector(".typePokemon");
-
 const getPokemons = data.pokemon;
 
-const renderPokemonsAll = () =>{
-  
+
+const arrayNinePokemon = []
+
+const arrayPokemon = (dataPokemons) =>{
+  const dataNinePokemon = dataPokemons.slice(0,9)
+ console.log(dataNinePokemon);
+ return dataNinePokemon
 }
 
-const searchPokemonsByTypes = () => {
-  showPokemons(filterPokemonsByType(typePokemon, getPokemons));
-};
+arrayPokemon(getPokemons)
 
-// const showPokemons = (pokemonData) => {
-  
-//   let pokemos = pokemonData.forEach((poke) => {
-//     let tbody = document.createElement("tbody");
-//     tbody.innerHTML = `
-//     <tr>
-//       <td>
-//         <p>#${poke.num}</p>
-//         <img src=${poke.img} alt=Logo ${poke.name}>
-//         <p>${poke.name}</p>
-//       </td>
-//     </tr>
-//     `;
-//     pokemonList.appendChild(tbody);
-//   });
+
+
+// const searchPokemonsByTypes = () => {
+//   showPokemons(filterPokemonsByType(typePokemon, getPokemons));
 // };
 
+const showPokemons = (pokemonData) => {
+  let pokemos = pokemonData.forEach((poke) => {
+    let div = document.createElement("div");
+    div.classList.add('right__list-ul')
+    div.innerHTML = `
+              <ul class="right__ul">
+                <li class="right__ul-li">
+                  <p>#${poke.num}</p>
+                  <img src=${poke.img} alt="">
+                  <span>${poke.name}</span>
+                </li>
+              </ul>
+    `;
+    pokemonList.append(div);
+  });
+};
+
+showPokemons(arrayPokemon(getPokemons))
+
 // Event Change
-typePokemon.addEventListener("change", searchPokemonsByTypes);
+// typePokemon.addEventListener("change", searchPokemonsByTypes);
