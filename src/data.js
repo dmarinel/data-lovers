3cfc9c214b4ed23ad216b356e11ab9d9414e2bbb
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-03-09 00:13:48 -0500

    ADD: create the function of search pokemon by name

--- src/data.js
+++ src/data.js
@@ -11,64 +11,92 @@ export const filterPokemonsByType = (typePokemon, dataPokemon) => {
 };
 
 export const filterPokemonByWeakness = (valueWeakness, dataPokemon) => {
-  if(valueWeakness.value === "all"){
-    return dataPokemon
-  }else{
-    const dataWeakness = dataPokemon.filter(poke => poke.weaknesses.includes(valueWeakness.value));
-  return dataWeakness
+  if (valueWeakness.value === "all") {
+    return dataPokemon;
+  } else {
+    const dataWeakness = dataPokemon.filter((poke) =>
+      poke.weaknesses.includes(valueWeakness.value)
+    );
+    return dataWeakness;
   }
-  
 };
-export const filterPokemonByResistant = (valueResistant, dataPokemon) =>{
-  if (valueResistant.value === 'all') {
-    return dataPokemon
-  }else {
-    const dataResistant = dataPokemon.filter(poke => poke.resistant.includes(valueResistant.value))
-  return dataResistant
+export const filterPokemonByResistant = (valueResistant, dataPokemon) => {
+  if (valueResistant.value === "all") {
+    return dataPokemon;
+  } else {
+    const dataResistant = dataPokemon.filter((poke) =>
+      poke.resistant.includes(valueResistant.value)
+    );
+    return dataResistant;
   }
-}
+};
 
-export const listTypesPokemon = (dataPokemon) =>{
+export const listTypesPokemon = (dataPokemon) => {
   // console.log(dataPokemon);
-  const arrayTypePokemon = []
-  for(let i=0; i<dataPokemon.length; i++ ){
-    const newData = dataPokemon[i].type
+  const arrayTypePokemon = [];
+  for (let i = 0; i < dataPokemon.length; i++) {
+    const newData = dataPokemon[i].type;
     for (let j = 0; j < newData.length; j++) {
       // console.log(newData[j]);
-      arrayTypePokemon.push(newData[j])
+      arrayTypePokemon.push(newData[j]);
     }
   }
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
   // constructor set => Use to remove duplicate elements from the array
   // sort => Metodo ordena los elementos de un array (corresponde a la posicion del string de acuerdo a su valor)
   // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-  const arrayListTypePokemon = [...new Set(arrayTypePokemon)].sort()
+  const arrayListTypePokemon = [...new Set(arrayTypePokemon)].sort();
   // console.log(arrayListTypePokemon);
-  return arrayListTypePokemon
-}
+  return arrayListTypePokemon;
+};
 
-export const  listWeaknessPokemon = (dataPokemon) =>{
+export const listWeaknessPokemon = (dataPokemon) => {
   // console.log(dataPokemon);
-  const arrayWeaknessPokemon = []
-  dataPokemon.map((poke)=>{
-    const newData = poke.weaknesses
-    newData.map(weak => arrayWeaknessPokemon.push(weak)
-    )
-  })
-  const arrayListWeaknessPokemon = [...new Set(arrayWeaknessPokemon)].sort()
+  const arrayWeaknessPokemon = [];
+  dataPokemon.map((poke) => {
+    const newData = poke.weaknesses;
+    newData.map((weak) => arrayWeaknessPokemon.push(weak));
+  });
+  const arrayListWeaknessPokemon = [...new Set(arrayWeaknessPokemon)].sort();
   // console.log(arrayListWeaknessPokemon);
-  return arrayListWeaknessPokemon
-}
+  return arrayListWeaknessPokemon;
+};
 
 export const listResistantPokemon = (dataPokemon) => {
   // console.log(dataPokemon);
-  const arrayResistantPokemon = []
-  dataPokemon.map((poke)=>{
-    const newData = poke.resistant
-    newData.map(resis => arrayResistantPokemon.push(resis))
-  })
+  const arrayResistantPokemon = [];
+  dataPokemon.map((poke) => {
+    const newData = poke.resistant;
+    newData.map((resis) => arrayResistantPokemon.push(resis));
+  });
   // console.log(arrayResistantPokemon);
-  const arrayListResistantPokemon = [...new Set(arrayResistantPokemon)].sort()
+  const arrayListResistantPokemon = [...new Set(arrayResistantPokemon)].sort();
   // console.log(arrayListResistantPokemon);
-  return arrayListResistantPokemon
+  return arrayListResistantPokemon;
+};
+
+export const top10PokemonByCp = (dataPokemon) => {
+  // const arrayCp = dataPokemon.map((e)=> e.stats["max-cp"])
+  const arrayCpOrder = dataPokemon.sort((a,b)=> b.stats["max-cp"]-a.stats["max-cp"])
+  const top10CpMax = arrayCpOrder.slice(0,10)
+  console.log(top10CpMax);
+  
+};
+
+export const searchPokemonByName = (searchInput, dataPokemon) =>{
+  // toLowerCase() => Metodo de string para convertir el input a minusculas 
+
+  let inputValue = searchInput.value.toLowerCase()
+  if (inputValue=== "all") {
+    return dataPokemon
+  }else{
+    const dataFilterByText = dataPokemon.filter((poke)=>{
+      
+      return poke.name.startsWith(inputValue)
+    })
+    console.log(dataFilterByText);
+    return dataFilterByText
+  }
 }
+
+
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import {
   listTypesPokemon,
   listWeaknessPokemon,
   listResistantPokemon,
+  top10PokemonByCp,
+  searchPokemonByName
 } from "./data.js";
 import data from "./data/pokemon/pokemon.js";
 
@@ -13,26 +15,39 @@ const pokemonList = document.querySelector("#pokemonList");
 const typePokemon = document.querySelector(".typePokemon");
 const previousButton = document.querySelector("#previousButton");
 const nextButton = document.querySelector("#nextButton");
+// Elements
 const infoPokemon = document.getElementById("infoPokemon");
+const searchText = document.getElementById("searchText");
+const upButton = document.getElementById("upButton")
+const downButton = document.getElementById("downButton")
 
 // Solo para pruebas
 const fiterType = document.querySelector("#filterType");
 const filterWeaknesses = document.querySelector("#filterWeaknesses");
 const filterResistant = document.querySelector("#filterResistant");
 
+// Get pokemons
+const getPokemons = data.pokemon;
+// Variables Globales
+let dataPoke = getPokemons;
+let cp1 = 0;
+let cp2 = 9;
+
+top10PokemonByCp(dataPoke);
+
 //Function to render information pokemon
-const renderInfoPokemonInit =() =>{
-  let figure = document.createElement("figure")
-  figure.classList.add("left__info-init")
-  figure.innerHTML =`
+const renderInfoPokemonInit = () => {
+  infoPokemon.innerHTML = "";
+  let figure = document.createElement("figure");
+  figure.classList.add("left__info-init");
+  figure.innerHTML = `
   <img src="./img/pokeball2.gif" width="250" height="300" alt="">
   <h2>Choose your <br> pokemon? </h2>
-  `
-  infoPokemon.appendChild(figure)
-}
+  `;
+  infoPokemon.appendChild(figure);
+};
 
 const renderInfoPokemon = (pokemon) => {
-  
   let pokemonType = pokemon.type.map((type) => {
     return `<img src="./img/${type}.png" alt=""></img>`;
   });
@@ -81,13 +96,6 @@ const renderInfoPokemon = (pokemon) => {
   infoPokemon.append(section);
 };
 
-// Get pokemons
-const getPokemons = data.pokemon;
-// Variables Globales
-let dataPoke = getPokemons;
-let cp1 = 0;
-let cp2 = 9;
-
 // funcion cantida de tipos
 listTypesPokemon(dataPoke);
 listWeaknessPokemon(dataPoke);
@@ -123,28 +131,26 @@ const filterPokemons = (e) => {
     : e.target.selectedIndex < 54
     ? (dataPoke = filterPokemonByResistant(typePokemon, getPokemons))
     : console.log("error");
-
-  // console.log(e.target.selectedIndex);
-  // if (e.target.selectedIndex < 19) {
-  //   console.log(filterType.label);
-  //   console.log(typePokemon.value);
-  //   dataPoke = filterPokemonsByType(typePokemon, getPokemons);
-  // } else if (e.target.selectedIndex < 36) {
-  //   console.log(filterWeaknesses.label);
-  //   console.log(typePokemon.value);
-  //   dataPoke = filterPokemonByWeakness(typePokemon, getPokemons);
-  // } else if (e.target.selectedIndex < 54) {
-  //   console.log(filterResistant.label);
-  //   console.log(typePokemon.value);
-  //   dataPoke = filterPokemonByResistant(typePokemon, getPokemons);
-  // } else {
-  //   console.log("error");
-  // }
-  // console.log(dataPoke);
-
+  e.target.selectedIndex = 55;
   nextPage(dataPoke);
   previousPage(dataPoke);
 };
+
+const searchPokemon = () =>{
+  dataPoke = searchPokemonByName(searchText, getPokemons)
+  nextPage(dataPoke);
+  previousPage(dataPoke);
+}
+
+const sortUp = (data) =>{
+  console.log(`click`);
+}
+
+const sortDown = (data) =>{
+  console.log(`clik Down`);
+}
+
+
 
 const showPokemons = (pokemonData) => {
   // innerHTML = '' => limpiar pokemonList
@@ -162,17 +168,23 @@ const showPokemons = (pokemonData) => {
               </ul>
     `;
     pokemonList.append(div);
-    
+
     div.addEventListener("click", () => renderInfoPokemon(poke));
+    renderInfoPokemonInit();
   });
-  renderInfoPokemonInit()
 };
 
 // ver los primeros 9 pokemos => inicializar
 showPokemons(dataPoke.slice(cp1, cp2));
 
+// Evento Click UP
+upButton.addEventListener("click", () => sortUp(dataPoke) )
+// Evento Click Down 
+downButton.addEventListener("click", () => sortDown(dataPoke))
 // Event Change
 typePokemon.addEventListener("change", filterPokemons);
+// Event keyUP
+searchText.addEventListener("keyup", searchPokemon )
 
 // event of pagination
 nextButton.addEventListener("click", () => nextNavigationPage(dataPoke));
